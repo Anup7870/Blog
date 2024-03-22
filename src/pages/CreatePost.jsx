9797e1079285ff1d2d8d1ef5ddde9fa1d79af83c
Anup7870 <kumaranup2505@gmail.com>
2024-03-22 17:14:51 +0530

    post

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -5,7 +5,10 @@ import "react-quill/dist/quill.snow.css";
 import { TextInput, Select, FileInput, Button, Alert } from "flowbite-react";
 import { app } from "../firebase.js";
 import { CircularProgressbar } from "react-circular-progressbar";
-
+import MuiAlert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import {
    getStorage,
    ref,
@@ -13,21 +16,24 @@ import {
    getDownloadURL,
 } from "firebase/storage";
 import "react-circular-progressbar/dist/styles.css";
+import { unstable_composeClasses } from "@mui/material";
 export default function CreatePost() {
    const {
       register,
       handleSubmit,
-
       formState: { errors },
       setValue,
+      reset,
    } = useForm();
+   const navigate = useNavigate();
    const [image, setImage] = useState(null);
    const [imageUploadProgress, setImageUploadProgress] = useState(null);
    const [imageUploadError, setImageUploadError] = useState(null);
-   // const [value, setValue] = useState(null);
-
+   const [postError, setPostError] = useState(null);
+   const [postSucess, setPostSucess] = useState(null);
+   const [downloadURL, setDownloadURL] = useState(null);
    // saving the image to firebase storage
-   const imageUplaod = async () => {
+   const imageUplaod = async (data) => {
       try {
          if (!image) {
             setImageUploadError("Please select an image to upload.");
@@ -43,18 +49,37 @@ export default function CreatePost() {
             "state_changed",
             (snapshot) => {
                const progress =
-                  (snapshot.bytesTransfer3red / snapshot.totalBytes) * 100;
+                  (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                setImageUploadProgress(progress.toFixed(0)); // to get the percentage we use toFixed(0) to remove the decimal
             },
             (error) => {
                setImageUploadError("Image upload failed. Please try again.");
                setImageUploadProgress(null); // if there is an error we clear the progress
+               return;
             },
             () => {
                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
                   setImageUploadProgress(null);
                   setImageUploadError(null);
-                  register("image", { value: downloadURL, required: true });
+
+                  setDownloadURL(downloadURL);
+                  console.log(downloadURL);
+                  try {
+                     const postData = { ...data, image: downloadURL };
+                     const post = axios
+                        .post("/api/post/create", postData)
+                        .then((data) => {
+                           console.log(data.data.post.slug);
+                           setPostSucess(true);
+                           setPostError(false);
+                           reset();
+                           console.log(post);
+                           navigate(`/post/${data.data.post.slug}`);
+                        });
+                  } catch (error) {
+                     console.log(error);
+                     setPostError(true);
+                  }
                });
             }
          );
@@ -67,17 +92,35 @@ export default function CreatePost() {
       setValue("content", content); // Set the value directly in the form hook
    };
    const handle = async (data) => {
-      await imageUplaod().then(() => {
-         console.log(data);
-      });
+      try {
+         const download = await imageUplaod();
+         console.log(downloadURL);
+         const postData = { ...data, image: downloadURL };
+         console.log(postData);
+         setPostSucess(true);
+         setPostError(false);
+         reset();
+      } catch (error) {
+         console.log(error);
+         setPostError(true);
+      }
    };
 
    return (
       <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+         {postSucess ? (
+            <MuiAlert severity='success'>Post uploaded sucessfully</MuiAlert>
+         ) : (
+            ""
+         )}
+         {postError ? <MuiAlert severity='error'>Unable to post</MuiAlert> : ""}
+         {/* <MuiAlert severity='error'>Unable to post</MuiAlert> */}
          <h1 className='text-center text-3xl my-7 font-semibold'>
             Create a post
          </h1>
-         <form onSubmit={handleSubmit(handle)} className='flex flex-col gap-4'>
+         <form
+            onSubmit={handleSubmit(imageUplaod)}
+            className='flex flex-col gap-4'>
             <div className='flex flex-col gap-4 sm:flex-row justify-between'>
                <TextInput
                   type='text'
@@ -87,7 +130,7 @@ export default function CreatePost() {
                   id='title'
                   className='flex-1'
                />
-               <Select {...register("category", { required: true })}>
+               <Select {...register("catergory", { required: true })}>
                   <option value='uncategorized'>Select a category</option>
                   <option value='javascript'>JavaScript</option>
                   <option value='reactjs'>React.js</option>
@@ -138,7 +181,10 @@ export default function CreatePost() {
                onChange={handleContentChange}
                required
             />
-            <Button type='submit' gradientDuoTone='purpleToPink'>
+            <Button
+               type='submit'
+               gradientDuoTone='purpleToPink'
+               disabled={imageUploadProgress}>
                Publish
             </Button>
          </form>
